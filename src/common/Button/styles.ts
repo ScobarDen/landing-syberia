4f1454f151942f5ddc10c93948a4c8c93f94611a
Denis Guschin <gdv-ps@yandex.ru>
2023-05-02 11:08:04 +0300

    style some components

--- src/common/Button/styles.ts
+++ src/common/Button/styles.ts
@@ -1,25 +1,24 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import { buttonBackground, textColorWhite } from '../../styles/colors';
 
-export const StyledButton = styled("button")<any>`
-  background: ${(p) => p.color || "#2e186a"};
-  color: ${(p) => (p.color ? "#2E186A" : "#fff")};
-  font-size: 1rem;
-  font-weight: 700;
-  width: 100%;
-  border: 1px solid #edf3f5;
-  border-radius: 4px;
-  padding: 13px 0;
-  cursor: pointer;
-  margin-top: 0.625rem;
-  max-width: 180px;
-  transition: all 0.3s ease-in-out;
-  box-shadow: 0 16px 30px rgb(23 31 114 / 20%);
+export const StyledButton = styled('button')<any>`
+	background: ${buttonBackground};
+	color: ${textColorWhite};
+	font-size: 0.8rem;
+	font-weight: 600;
+	border-radius: 4px;
+	padding: 7px 15px;
+	cursor: pointer;
+	box-shadow: none;
+	transition: all 0.3s ease-in-out;
+	outline: none;
+	border: none;
 
-  &:hover,
-  &:active,
-  &:focus {
-    color: #fff;
-    border: 1px solid rgb(255, 130, 92);
-    background-color: rgb(255, 130, 92);
-  }
+	&:hover,
+	&:active,
+	&:focus {
+		color: ${textColorWhite};
+		border: 1px solid ${buttonBackground};
+		background-color: ${buttonBackground};
+	}
 `;
