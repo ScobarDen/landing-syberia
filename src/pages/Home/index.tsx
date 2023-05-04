b88971a9de2e460b92d921519a3245f6d0cfd6dc
Denis Guschin <gdv-ps@yandex.ru>
2023-05-04 16:05:27 +0300

    add lazyLoad to priceBlock

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,11 @@
 import { lazy } from 'react';
 import Container from '../../common/Container';
-import PriceBlock from '../../components/PriceBlock';
 import HeaderBlockContent from '../../content/HeaderBlockContent.json';
 import PriceBlockContent from '../../content/PriceBlockContent.json';
 
 const HeaderContentBlock = lazy(() => import('../../components/HeaderContentBlock'));
+
+const PriceBlock = lazy(() => import('../../components/PriceBlock'));
 
 const Home = () => {
 	return (
